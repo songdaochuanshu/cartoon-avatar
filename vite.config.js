e8d50e3fa99751fdefd84e32b4b73bc02e9748e8
松岛川树 <songdaochuanshu@gmail.com>
2022-03-21 14:22:35 +0800

    delete workflows

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: '/cartoon-avatar/'
+  plugins: [vue()]
+  // base: '/cartoon-avatar/'
 })
